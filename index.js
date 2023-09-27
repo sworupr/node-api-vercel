9ebd105eea4cd3dc6f24477e1a3fb3051668f524
Sworup Ranjit <sworup@sworups-mbp.mynetworksettings.com>
2023-09-26 22:31:12 -0400

    fuckitall

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser')
 // const cors = require('cors')
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://sworup:c1MNXl0hxt2EEAHg@cluster0.iqctmc7.mongodb.net/?retryWrites=true&w=majority', {
+MONGO_URI = 'mongodb+srv://sworup:c1MNXl0hxt2EEAHg@cluster0.iqctmc7.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
